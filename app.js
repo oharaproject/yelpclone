7f5f5311b0e79d4c93f5a73d8d57955bdb5a7e0f
Muhdin Dai <muhdindai07@gmail.com>
2025-06-22 19:17:03 +0800

    fix CRUD

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
@@ -6,7 +7,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // model
 const Place = require("./models/place");
@@ -70,5 +73,40 @@ app.get("/places/:id", async (req, res) => {
     return res.status(404).send("Place not found");
   } else {
     res.render("places/show", { place });
+  }
+});
+
+// Edit place route
+app.get("/places/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const place = await Place.findById(id);
+  if (!place) {
+    return res.status(404).send("Place not found");
+  } else {
+    res.render("places/edit", { place });
+  }
+});
+
+// Update place route
+app.put("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  await Place.findByIdAndUpdate(id, { ...req.body.place })
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send("Error updating place");
+    });
+});
+
+app.delete("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Place.findByIdAndDelete(id);
+    res.redirect("/places");
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Error deleting place");
   }
 });
